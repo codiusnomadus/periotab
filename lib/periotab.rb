4279a2ba6cf5c5704caaf3ac4b5169c08afc028e
Codius Nomadus <thecodenomad@gmail.com>
2017-10-18 00:57:54 +0800

    added gem installation file to gitignore

--- lib/periotab.rb
+++ lib/periotab.rb
@@ -1,7 +1,10 @@
 require "periotab/version"
 
-module Periotab
-  class Hola
-  	puts "Hello world!"
-  end
+class Periotab
+	def self.hi(language = 'english')
+		translator = Translator.new
+		translator.hi
+	end
 end
+
+require 'periotab/translator'
--- lib/periotab/version.rb
+++ lib/periotab/version.rb
@@ -1,3 +1,3 @@
-module Periotab
+class Periotab
   VERSION = "0.1.0"
 end
